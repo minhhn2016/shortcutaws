e4ed67cf97f2608db99601dae24aaf3b2270cad7
minhhn2016 <minh.hn2016@gmail.com>
2019-06-17 11:02:12 +0300

    modified cognito configs

--- js/config.js
+++ js/config.js
@@ -1,10 +1,10 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'eu-west-1_apOv3IQEb', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '4o5o4imo3p1lr5bepfm3t0i1g8', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: '' // e.g. us-east-2
     },
     api: {
-        invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+        invokeUrl: 'eu-west-1' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
