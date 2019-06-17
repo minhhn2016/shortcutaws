b75e061e47ac846f83b656ce222e37af4023dd8b
minhhn2016 <minh.hn2016@gmail.com>
2019-06-17 11:06:17 +0300

    fixed region config for cognito

--- js/config.js
+++ js/config.js
@@ -2,9 +2,9 @@ window._config = {
     cognito: {
         userPoolId: 'eu-west-1_apOv3IQEb', // e.g. us-east-2_uXboG5pAb
         userPoolClientId: '4o5o4imo3p1lr5bepfm3t0i1g8', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        region: 'eu-west-1' // e.g. us-east-2
     },
     api: {
-        invokeUrl: 'eu-west-1' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+        invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
